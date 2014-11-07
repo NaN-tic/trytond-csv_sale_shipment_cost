22c6996ff3639536741dcd3727718e09982183b0
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:21:55 +0100

    Cleanup tests

--- tests/test_csv_sale_shipment_cost.py
+++ tests/test_csv_sale_shipment_cost.py
@@ -7,24 +7,16 @@ import sys
 import trytond.tests.test_tryton
 import unittest
 
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class CSVSaleShipmentCostTestCase(unittest.TestCase):
-    '''
-    Test CSV Sale Shipment Cost module.
-    '''
+    'Test CSV Sale Shipment Cost module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('csv_sale_shipment_cost')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
